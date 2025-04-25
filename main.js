9744b985f2600154a0734fc02a3909c48df5a497
1k2fg <160850524+1k2fg@users.noreply.github.com>
2025-04-25 10:54:35 -0300

    o melhor,lucas e ryan

---
+++ main.js
@@ -0,0 +1,8 @@
+let ranomNumber =
+Math.floor(Math.random() *10) + 1;
+let attempts = 0;
+
+function checkGuess() {
+    const userguess = parseInt(document.getElementById('guess'). ariaValueMax)
+    attempts
+}
